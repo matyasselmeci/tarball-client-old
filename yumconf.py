5aedfe8f45fe95e41de231a4b5861fbf55f3e34c
Matyas Selmeci <matyas@cs.wisc.edu>
2015-02-03 18:30:17 -0600

    Also set locale to C when doing rpm installs, for consistency (SOFTWARE-1769)

--- yumconf.py
+++ yumconf.py
@@ -197,7 +197,9 @@ class YumConfig(object):
                     "--justdb",
                     "--root", installroot]
             cmd2 += rpms
-            return subprocess.call(cmd2)
+            env = os.environ.copy()
+            env.update({'LANG': 'C', 'LC_ALL': 'C'})
+            return subprocess.call(cmd2, env=env)
         finally:
             shutil.rmtree(rpm_dir, ignore_errors=True)
 
